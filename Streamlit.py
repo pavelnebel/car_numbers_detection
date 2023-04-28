13c1d9363d7ec6432fbab77d14ec06b6a05412a1
PavelnebeL <nebel7496@gmail.com>
2023-04-28 19:44:40 +0300

    Рабочий сервис. Выводится номер

--- Streamlit.py
+++ Streamlit.py
@@ -3,26 +3,28 @@ import glob
 import time
 import streamlit as st
 import warnings
+import numpy as np
+import cv2
 warnings.filterwarnings('ignore')
 
 
-tich_file= glob.glob('./Object_Detection/*')
+#uploaded_images= glob.glob('./Object_Detection/*')
 paths_nomer= './model1_nomer.tflite'
 paths_resnet= './model_resnet.tflite'
-
 
 st.set_page_config(page_title="Распознавание номеров", layout="wide", page_icon="random")
 st.header('Сервис по распознаванию автомобильных номеров')
 
-uploaded_images = st.file_uploader("Choose video", type=["jpg", "png", "jpeg"])
+uploaded_images = st.file_uploader("Choose image", type=["jpg", "png", "jpeg"])
 
 if uploaded_images is not None: # run only when user uploads video
 
-    vid = uploaded_images.name
+    file_bytes = np.asarray(bytearray(uploaded_images.read()), dtype=np.uint8)
+    uploaded_images = cv2.imdecode(file_bytes, 1)
 
     # Блок распознавания
     exec_time = time.time()
-    number, detection_img = detection_recognition(tich_file, paths_nomer, paths_resnet)
+    number, detection_img = detection_recognition(uploaded_images, paths_nomer, paths_resnet)
 
     # st.markdown(
     #     number,
@@ -30,4 +32,4 @@ if uploaded_images is not None: # run only when user uploads video
     # )
     st.header(f'Распознанный номер: {number[0]}')
 
-    st.image(uploaded_images, channels="BGR")
+    st.image(detection_img, channels="BGR")
--- detection.py
+++ detection.py
@@ -1,7 +1,6 @@
 import os
 import cv2
-import numpy as np
-from typing import Any, Dict, List, Optional, Tuple
+
 import numpy as np
 from skimage.feature import canny
 from skimage.transform import hough_line, hough_line_peaks
@@ -16,20 +15,16 @@ import itertools
 import tensorflow as tf
 import requests,io
 
-def detection_recognition (tich_file, paths_nomer, paths_resnet):
-    #tich_file = glob.glob('./Object_Detection/test/*')
+def detection_recognition (upload_images, paths_nomer, paths_resnet):
 
-
-    img_name1=tich_file[0]
-    image = cv2.imread(img_name1)
-
-    i=0
-    img_name1=tich_file[i]
- #   path = img_name1
-    image0 = cv2.imread(img_name1, 1)
-    image_height, image_width, _ = image0.shape
-    image = cv2.resize(image0, (1024,1024))
+    # img_name1=upload_images
+    # image = cv2.imread(img_name1)
+    #
+    # image0 = cv2.imread(img_name1, 1)
+    image_height, image_width, _ = upload_images.shape
+    image = cv2.resize(upload_images, (1024,1024))
     image = image.astype(np.float32)
+
 
    # paths = './model_resnet.tflite'
     interpreter = tf.lite.Interpreter(model_path=paths_resnet)
@@ -44,13 +39,11 @@ def detection_recognition (tich_file, paths_nomer, paths_resnet):
     net_out_value2 = interpreter.get_tensor(output_details[1]['index'])
     net_out_value3 = interpreter.get_tensor(output_details[2]['index'])
     net_out_value4 = interpreter.get_tensor(output_details[3]['index'])
-    img = image0
+    img = upload_images
 
-    razmer=img.shape
 
     img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #Converts from one colour space to the other
     img3=img[:,:,:]
-
 
     box_x =int( detection[0,0,0] * image_height)
     box_y = int(detection[0,0,1] * image_width)
@@ -59,19 +52,16 @@ def detection_recognition (tich_file, paths_nomer, paths_resnet):
 
     cv2.rectangle(img2, ( box_y,box_x), (box_height,box_width ), (230, 230, 21), thickness=5)
 
-    image_detection_car_num = img2
-
-
 
     net_out_value3
 
-    image = image0[box_x:box_width,box_y:box_height,:]
+    image = upload_images[box_x:box_width,box_y:box_height,:]
     img2 = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
+    image_detection_car_num = img2
+
     grayscale = rgb2gray(image)
-
     edges = canny(grayscale, sigma=3.0)
-
     out, angles, distances = hough_line(edges)
     h, theta, d=out, angles, distances
     angle_step = 0.5 * np.diff(theta).mean()
@@ -151,7 +141,7 @@ def detection_recognition (tich_file, paths_nomer, paths_resnet):
     pred_texts = decode_batch(net_out_value)
     pred_texts
     #['A999AA99']
-
+    print(pred_texts)
 
     fig = plt.figure(figsize=(10, 10))
     outer = gridspec.GridSpec(2, 1, wspace=10, hspace=0.1)
@@ -173,91 +163,91 @@ def detection_recognition (tich_file, paths_nomer, paths_resnet):
     for h in np.arange(-0.5, len(letters) + 1 + 0.5, 1):
         ax2.axhline(h, linestyle='-', color='k', alpha=0.5, linewidth=1)
 
-    for i in range(len(tich_file)):
-        img_name1=tich_file[i]
-        path = img_name1
-        image0 = cv2.imread(img_name1, 1)
-        image_height, image_width, _ = image0.shape
-        image = cv2.resize(image0, (1024,1024))
-        image = image.astype(np.float32)
-        #paths='./model_resnet.tflite'
-        interpreter = tf.lite.Interpreter(model_path=paths_resnet)
+ #   for i in range(len(upload_images)):
+    # img_name1=upload_images
+    # path = img_name1
+    # image0 = cv2.imread(img_name1, 1)
+    # image_height, image_width, _ = image0.shape
+    image = cv2.resize(upload_images, (1024,1024))
+    image = image.astype(np.float32)
+    #paths='./model_resnet.tflite'
+    interpreter = tf.lite.Interpreter(model_path=paths_resnet)
+    interpreter.allocate_tensors()
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    X_data1=np.float32(image.reshape(1,1024, 1024,3))
+    input_index = (interpreter.get_input_details()[0]['index'])
+    interpreter.set_tensor(input_details[0]['index'], X_data1)
+    interpreter.invoke()
+    detection = interpreter.get_tensor(output_details[0]['index'])
+    net_out_value2 = interpreter.get_tensor(output_details[1]['index'])
+    net_out_value3 = interpreter.get_tensor(output_details[2]['index'])
+    net_out_value4 = interpreter.get_tensor(output_details[3]['index'])
+    img = upload_images
+    razmer=img.shape
+
+    img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #Converts from one colour space to the other
+    img3=img[:,:,:]
+
+    box_x =int( detection[0,0,0] * image_height)
+    box_y = int(detection[0,0,1] * image_width)
+    box_width =int( detection[0,0,2] * image_height)
+    box_height = int(detection[0,0,3] * image_width)
+    if  np.min(detection[0,0,:])>=0:
+        cv2.rectangle(img2, ( box_y,box_x), (box_height,box_width ), (230, 230, 21), thickness=5)
+
+        # plt.imshow(img2)
+        # plt.xticks([]), plt.yticks([])  # Hides the graph ticks and x / y axis
+        # plt.show()
+        image = img3[box_x:box_width,box_y:box_height,:]
+        grayscale = rgb2gray(image)
+        edges = canny(grayscale, sigma=3.0)
+        out, angles, distances = hough_line(edges)
+        _, angles_peaks, _ = hough_line_peaks(out, angles, distances, num_peaks=20)
+        angle=np.mean(np.rad2deg(angles_peaks))
+        if 0 <= angle <= 90:
+            rot_angle = angle - 90
+        elif -45 <= angle < 0:
+            rot_angle = angle - 90
+        elif -90 <= angle < -45:
+            rot_angle = 90 + angle
+        if abs(rot_angle)>20:
+            rot_angle=0
+        rotated = rotate(image, rot_angle, resize=True)*255
+        rotated =rotated.astype(np.uint8)
+        rotated1=rotated[:,:,:]
+        minus=np.abs(int(np.sin(np.radians(rot_angle))*rotated.shape[0]))
+        if rotated.shape[1]/rotated.shape[0]<2 and minus >10:
+
+            rotated1=rotated[minus:-minus,:,:]
+        lab= cv2.cvtColor(rotated1, cv2.COLOR_BGR2LAB)
+        l, a, b = cv2.split(lab)
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+        cl = clahe.apply(l)
+        limg = cv2.merge((cl,a,b))
+        final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+        #paths='./model1_nomer.tflite'
+        interpreter = tf.lite.Interpreter(model_path=paths_nomer)
         interpreter.allocate_tensors()
+        # Get input and output tensors.
         input_details = interpreter.get_input_details()
         output_details = interpreter.get_output_details()
-        X_data1=np.float32(image.reshape(1,1024, 1024,3))
+    #         img =rotated1
+        img =final # лучше работает при плохом освещении
+
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (128,64))
+        img = img.astype(np.float32)
+        img /= 255
+
+        img1=img.T
+        img1.shape
+        X_data1=np.float32(img1.reshape(1,128, 64,1))
         input_index = (interpreter.get_input_details()[0]['index'])
         interpreter.set_tensor(input_details[0]['index'], X_data1)
+
         interpreter.invoke()
-        detection = interpreter.get_tensor(output_details[0]['index'])
-        net_out_value2 = interpreter.get_tensor(output_details[1]['index'])
-        net_out_value3 = interpreter.get_tensor(output_details[2]['index'])
-        net_out_value4 = interpreter.get_tensor(output_details[3]['index'])
-        img = image0
-        razmer=img.shape
 
-        img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #Converts from one colour space to the other
-        img3=img[:,:,:]
-
-        box_x =int( detection[0,0,0] * image_height)
-        box_y = int(detection[0,0,1] * image_width)
-        box_width =int( detection[0,0,2] * image_height)
-        box_height = int(detection[0,0,3] * image_width)
-        if  np.min(detection[0,0,:])>=0:
-            cv2.rectangle(img2, ( box_y,box_x), (box_height,box_width ), (230, 230, 21), thickness=5)
-
-            # plt.imshow(img2)
-            # plt.xticks([]), plt.yticks([])  # Hides the graph ticks and x / y axis
-            # plt.show()
-            image = img3[box_x:box_width,box_y:box_height,:]
-            grayscale = rgb2gray(image)
-            edges = canny(grayscale, sigma=3.0)
-            out, angles, distances = hough_line(edges)
-            _, angles_peaks, _ = hough_line_peaks(out, angles, distances, num_peaks=20)
-            angle=np.mean(np.rad2deg(angles_peaks))
-            if 0 <= angle <= 90:
-                rot_angle = angle - 90
-            elif -45 <= angle < 0:
-                rot_angle = angle - 90
-            elif -90 <= angle < -45:
-                rot_angle = 90 + angle
-            if abs(rot_angle)>20:
-                rot_angle=0
-            rotated = rotate(image, rot_angle, resize=True)*255
-            rotated =rotated.astype(np.uint8)
-            rotated1=rotated[:,:,:]
-            minus=np.abs(int(np.sin(np.radians(rot_angle))*rotated.shape[0]))
-            if rotated.shape[1]/rotated.shape[0]<2 and minus >10:
-
-                rotated1=rotated[minus:-minus,:,:]
-            lab= cv2.cvtColor(rotated1, cv2.COLOR_BGR2LAB)
-            l, a, b = cv2.split(lab)
-            clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
-            cl = clahe.apply(l)
-            limg = cv2.merge((cl,a,b))
-            final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
-            #paths='./model1_nomer.tflite'
-            interpreter = tf.lite.Interpreter(model_path=paths_nomer)
-            interpreter.allocate_tensors()
-            # Get input and output tensors.
-            input_details = interpreter.get_input_details()
-            output_details = interpreter.get_output_details()
-    #         img =rotated1
-            img =final # лучше работает при плохом освещении
-
-            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            img = cv2.resize(img, (128,64))
-            img = img.astype(np.float32)
-            img /= 255
-
-            img1=img.T
-            img1.shape
-            X_data1=np.float32(img1.reshape(1,128, 64,1))
-            input_index = (interpreter.get_input_details()[0]['index'])
-            interpreter.set_tensor(input_details[0]['index'], X_data1)
-
-            interpreter.invoke()
-
-            net_out_value = interpreter.get_tensor(output_details[0]['index'])
-            pred_texts = decode_batch(net_out_value)
+        net_out_value = interpreter.get_tensor(output_details[0]['index'])
+        pred_texts = decode_batch(net_out_value)
     return (pred_texts, image_detection_car_num)
